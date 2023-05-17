9f20a89c50ad96eb35fff75a8b0c4b4bb4515ab7
BLADE-HEDA <nkwetakembrunodudu@gmail.com>
2023-05-17 23:27:43 +0100

    build with success

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,8 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBY_A3LwY8pVyRHu_20JIqRqvEmtQRc0xo",
@@ -13,11 +13,11 @@ const firebaseConfig = {
     appId: "1:296167296869:web:3898721b457d2891c89b8c",
     measurementId: "G-KF9LB8G9F6"
   };
-  
+
   firebase.initializeApp(firebaseConfig);
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 
 

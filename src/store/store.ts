06503820efa1007f6b007a731d6be261e8e76c4a
BLADE-HEDA <nkwetakembrunodudu@gmail.com>
2023-07-03 13:03:03 +0100

    reconfigured firebase

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+// link your App to firebase 
+import { initializeApp } from 'firebase/app';
+import firebase from "../firebase/firebase"
 // Import your reducers
 import counterReducer from "./counterSlice";
+// Initialize Firebase app
+const firebaseApp = initializeApp(firebase);
 
 const store = configureStore({
   reducer: {

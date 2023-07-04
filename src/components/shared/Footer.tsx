d5258f06d4c404d10b4b7d46df779e41a57e4809
BLADE-HEDA <you@example.com>
2023-07-04 16:54:36 +0100

    initial changes after cloning

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <main className='px-5 flex justify-between  mb-2'>
+    <main className='px-5 flex justify-between '>
         <div className="text-[black] font-[600] ">DRTech © 2023 </div>
         <div className="text-[black]">Privacy policy | T&C</div>
     </main>
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,10 +1,10 @@
 // import React from 'react'
-import logo from "../../../public/Private Journal 2.png"
+import logo from "../../assets/Private Journal 2.png"
 
 const Navbar = () => {
   return (
     <div className="bg-[#000] flex  py-3 px-5 justify-between  ">
-        <div className="logo  ">
+        <div className="logo  cursor-pointer ">
             <img src={logo} alt="logo" />
         </div>
         <p className=" text-[#FFF] text-[1.6em] font-[700] text-center  " >My Private Diary</p>

2eb1954b4bc8dff528a7129a53cea33ee5fd19d3
BLADE-HEDA <you@example.com>
2023-07-05 15:31:15 +0100

    changes done to my code

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,19 +1,19 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from '../store/counterSlice';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { increment, decrement } from '../store/counterSlice';
 
-const Counter = () => {
-  // const count = useSelector((state) => state.counter.value);
-  const count = useSelector((state: any) => state.counter.value);
-  const dispatch = useDispatch();
+// const Counter = () => {
+//   // const count = useSelector((state) => state.counter.value);
+//   const count = useSelector((state: any) => state.counter.value);
+//   const dispatch = useDispatch();
 
-  return (
-    <div>
-       <div className='text-3xl font-bold text-blue-600' >Welcome to Diary App </div>
-      <div>Count: {count}</div>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//        <div className='text-3xl font-bold text-blue-600' >Welcome to Diary App </div>
+//       <div>Count: {count}</div>
+//       <button onClick={() => dispatch(increment())}>Increment</button>
+//       <button onClick={() => dispatch(decrement())}>Decrement</button>
+//     </div>
+//   );
+// };
 
-export default Counter;
+// export default Counter;

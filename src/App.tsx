89aa1592d1d9f61cbe1d37238c510dfab5e6b629
BLADE-HEDA <you@example.com>
2023-07-05 15:40:38 +0100

    changes done to my code once morew

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
-// import Counter from './components/Counter'
 import SigninFacebookGoogle from './components/Aunthentcation /SigninFacebookGoogle'
 import Footer from './components/shared/Footer'
 import Navbar from './components/shared/Navbar'
@@ -12,7 +8,6 @@ function App() {
 
   return (
     <BrowserRouter >
-    {/* <Counter/> */}
     <Navbar/>
     <Routes> 
     <Route path='/' element={<SigninFacebookGoogle/>}/> 

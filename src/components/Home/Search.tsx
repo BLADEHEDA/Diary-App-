f0341afafaba901c1cdd7bfde27a0de8772a750d
BLADE-HEDA <you@example.com>
2023-07-19 12:41:51 +0100

    refactoring ongoing

--- src/components/Home/Search.tsx
+++ src/components/Home/Search.tsx
@@ -1,35 +1,42 @@
-// import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch,faFilter } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faFilter } from '@fortawesome/free-solid-svg-icons';
 
-const Search = ({onSearch} ) => {
-  const [ searchText ,setSearchText]= useState('');
-  // handle the input change of the search input 
-  const handleInputChange = (event) => {
+interface SearchProps {
+  onSearch: (searchText: string) => void;
+}
+
+const Search: React.FC<SearchProps> = ({ onSearch }) => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     setSearchText(value);
     onSearch(value); // Pass the search text to the parent component
   };
 
-return (
-    <main  >
-        <section className="flex justify-between px-3 mt-5  mb-1 ">
-        <div className="bg-[white] ">
-            <input
-             type="text " 
-             className='bg-[white]  input text-red-600::placeholder'  
-             placeholder="Type here to search"
-             value={searchText} 
-             onChange={handleInputChange}
-             />
-      
+  return (
+    <main>
+      <section className="flex justify-between px-3 mt-5 mb-1">
+        <div className="bg-[white]">
+          <input
+            type="text"
+            className="bg-[white] input text-red-600::placeholder"
+            placeholder="Type here to search"
+            value={searchText}
+            onChange={handleInputChange}
+          />
         </div>
-        <div className=""><FontAwesomeIcon className='text-[1.4em] ' icon={faSearch } /></div>
-        <div className=""><FontAwesomeIcon className='text-[1.4em] ' icon={faFilter} /></div>
-        </section>
-        <div className="border-b  mx-3 border-[black] mb-5"></div>
+        <div className="">
+          <FontAwesomeIcon className="text-[1.4em]" icon={faSearch} />
+        </div>
+        <div className="">
+          <FontAwesomeIcon className="text-[1.4em]" icon={faFilter} />
+        </div>
+      </section>
+      <div className="border-b mx-3 border-[black] mb-5"></div>
     </main>
-  )
-}
-export default Search 
+  );
+};
+
+export default Search;
--- src/components/shared/Home.tsx
+++ src/components/shared/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import DiaryItem from '../Home/DiaryItem';
 import HomeHeader from '../Home/HomeHeader';
@@ -7,55 +7,63 @@ import { db } from '../../firebase/firebase';
 import { collection, getDocs } from 'firebase/firestore';
 import book from "../../assets/pic1.png"
 
+interface DiaryEntry {
+  id: string;
+  category: string;
+  description: string;
+  selectedFile: string;
+  date: string;
+  isPublic: boolean;
+}
+
 const Home = () => {
-  const [diary, setDiary] = useState([]);
+  const [diary, setDiary] = useState<DiaryEntry[]>([]);
   // subjected to changes 
-  // States for the handle search fnctionality 
-  const [filteredDiary, setFilteredDiary] = useState([]);
+  // States for the handle search functionality 
+  const [filteredDiary, setFilteredDiary] = useState<DiaryEntry[]>([]);
 
   const fetchPost = async () => {
     await getDocs(collection(db, 'diaryEntries')).then((querySnapshot) => {
-      const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      const newData = querySnapshot.docs.map((doc) => ({ ...doc.data() } as DiaryEntry));
       setDiary(newData);
       setFilteredDiary(newData);
       console.log(newData);
     });
   };
-
+  
   useEffect(() => {
     fetchPost();
   }, []);
   // handle the search functionality 
-  const handleSearch = (searchText) => {
+  const handleSearch = (searchText: string) => {
     // filter the data 
     const filteredData = diary.filter(
       (item) =>
-      // make sure all the texts are considered
+        // make sure all the texts are considered
         item.category.toLowerCase().includes(searchText.toLowerCase()) ||
         item.description.toLowerCase().includes(searchText.toLowerCase())
-       
     );
     setFilteredDiary(filteredData);
   };
 
   return (
     <main className="bg-[white] mb-[3em] ">
-    <Navbar head="Home" vector={localStorage.getItem('pic')} />
-    <section className="px-3">
-      <HomeHeader />
-      <Search onSearch={handleSearch} />
-      {filteredDiary.map((item) => (
-        <DiaryItem
-          key={item.id}
-          src={item.selectedFile || book}
-          title={item.category}
-          date={item.date}
-          type={item.isPublic ? 'Public' : 'Private'}
-          content={item.description}
-        />
-      ))}
-    </section>
-  </main>
+      <Navbar head="Home" vector={localStorage.getItem('pic')} />
+      <section className="px-3">
+        <HomeHeader />
+        <Search onSearch={handleSearch} />
+        {filteredDiary.map((item) => (
+          <DiaryItem
+            key={item.id}
+            src={item.selectedFile || book}
+            title={item.category}
+            date={item.date}
+            type={item.isPublic ? 'Public' : 'Private'}
+            content={item.description}
+          />
+        ))}
+      </section>
+    </main>
   );
 };
 

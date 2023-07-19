67ad1c728e76e241dce65a2dc161700874bff2ce
BLADE-HEDA <you@example.com>
2023-07-19 12:21:49 +0100

    search implemented , optimizarion left

--- src/components/shared/Home.tsx
+++ src/components/shared/Home.tsx
@@ -9,11 +9,15 @@ import book from "../../assets/pic1.png"
 
 const Home = () => {
   const [diary, setDiary] = useState([]);
+  // subjected to changes 
+  // States for the handle search fnctionality 
+  const [filteredDiary, setFilteredDiary] = useState([]);
 
   const fetchPost = async () => {
     await getDocs(collection(db, 'diaryEntries')).then((querySnapshot) => {
       const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
       setDiary(newData);
+      setFilteredDiary(newData);
       console.log(newData);
     });
   };
@@ -21,26 +25,37 @@ const Home = () => {
   useEffect(() => {
     fetchPost();
   }, []);
+  // handle the search functionality 
+  const handleSearch = (searchText) => {
+    // filter the data 
+    const filteredData = diary.filter(
+      (item) =>
+      // make sure all the texts are considered
+        item.category.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.description.toLowerCase().includes(searchText.toLowerCase())
+       
+    );
+    setFilteredDiary(filteredData);
+  };
 
   return (
     <main className="bg-[white] mb-[3em] ">
-     
-        <Navbar head="Home" vector={localStorage.getItem('pic')} />
-        <section className='px-3' >
-        <HomeHeader />
-        <Search />
-        {diary.map((item) => (
-          <DiaryItem
-            key={item.id}
-            src={item.selectedFile ||  book } // Set the default image source here
-            title={item.category}
-            date={item.date}
-            type={item.isPublic ? 'Public' : 'Private'}
-            content={item.description}
-          />
-        ))}
-      </section>
-    </main>
+    <Navbar head="Home" vector={localStorage.getItem('pic')} />
+    <section className="px-3">
+      <HomeHeader />
+      <Search onSearch={handleSearch} />
+      {filteredDiary.map((item) => (
+        <DiaryItem
+          key={item.id}
+          src={item.selectedFile || book}
+          title={item.category}
+          date={item.date}
+          type={item.isPublic ? 'Public' : 'Private'}
+          content={item.description}
+        />
+      ))}
+    </section>
+  </main>
   );
 };
 

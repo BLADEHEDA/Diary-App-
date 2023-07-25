570ef9918f72bae0a10c61db5d46bc55268ac992
BLADE-HEDA <you@example.com>
2023-07-25 14:05:14 +0100

    dine with the ui

--- src/components/Home/Filtermodal.tsx
+++ src/components/Home/Filtermodal.tsx
@@ -19,20 +19,24 @@ const Filtermodal: React.FC<FiltermodalProps> = ({ onClose }) => {
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-8 rounded-lg shadow-lg w-3/4">
-        <h2 className="text-black text-xl font-bold mb-6">Filter your diary entries</h2>
+      <div className="bg-white pb-8 h-full  rounded shadow-lg absolute top-0 right-0">
+        <div className="bg-[black] text-[white]  text-2xl font-[700] pl-5 py-3  mb-5 ">
+            Filter
+        </div>
+        <div className="px-5 "> 
+        <h2 className="text-black text-[1.5em] font-bold mb-6 ">Filter your diary entries</h2>
         <form className="flex flex-col" onSubmit={(e) => e.preventDefault()}>
-          <div className="mb-4">
-            <label htmlFor="category" className="text-black font-semibold">
+          <div className="mb-5">
+            <label htmlFor="category" className="text-black text-[1.2em] italic ">
               Category
             </label>
             <select
               id="category"
-              className="w-full border border-black rounded px-2 py-1"
+              className="w-full border border-black rounded px-2 py-3 bg.[white]  "
               value={category}
               onChange={(e) => setCategory(e.target.value)}
             >
-              <option value="All">All</option>
+              <option className="hidden" value="All">Categories</option>
               <option value="Fun">Fun</option>
               <option value="Home">Home</option>
               <option value="Family">Family</option>
@@ -42,37 +46,38 @@ const Filtermodal: React.FC<FiltermodalProps> = ({ onClose }) => {
               <option value="Others">Others</option>
             </select>
           </div>
-          <div className="mb-4">
-            <label htmlFor="startDate" className="text-black font-semibold">
+          <div className="mb-5">
+            <label htmlFor="startDate" className="text-black text-[1.2em] italic">
               Start Date
             </label>
             <input
               type="date"
               id="startDate"
-              className="w-full border border-black rounded px-2 py-1"
+              className="w-full border border-black rounded px-2 py-2"
               value={startDate}
               onChange={(e) => setStartDate(e.target.value)}
             />
           </div>
-          <div className="mb-4">
-            <label htmlFor="endDate" className="text-black font-semibold">
+          <div className="mb-5">
+            <label htmlFor="endDate" className="text-black text-[1.2em] italic">
               End Date
             </label>
             <input
               type="date"
               id="endDate"
-              className="w-full border border-black rounded px-2 py-1"
+              className="w-full border border-black rounded px-2 py-2"
               value={endDate}
               onChange={(e) => setEndDate(e.target.value)}
             />
           </div>
           <button
-            className="bg-black text-white px-4 py-2 rounded"
+            className="bg-black text-white px-4 py-4 mt-3 rounded"
             onClick={handleFilter}
           >
             Filter
           </button>
         </form>
+        </div>
       </div>
     </div>
   );
